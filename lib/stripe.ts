5fb6d1e1d9e6029396ee8707f532a5849382d4ec
realestate-p2v <realestatephoto2video@gmail.com>
2026-01-28 12:45:14 -0500

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,10 +1,5 @@
-import Stripe from "stripe";
+import { loadStripe } from "@stripe/stripe-js";
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error("STRIPE_SECRET_KEY is missing from environment variables");
-}
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-06-20", // or your preferred version
-  typescript: true,
-});
+export const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
