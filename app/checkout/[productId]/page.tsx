42d4db7ce97deaf1e47effeace08ad7e832f5b39
realestate-p2v <realestatephoto2video@gmail.com>
2026-01-31 09:32:27 -0500

    Update page.tsx

--- app/checkout/[productId]/page.tsx
+++ app/checkout/[productId]/page.tsx
@@ -117,3 +117,13 @@ export default async function CheckoutPage({
     </div>
   );
 }
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4VFMMPJDBN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4VFMMPJDBN');
+</script>
