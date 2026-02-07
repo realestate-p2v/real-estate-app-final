4d285b73cee50bdd03e80094f8bb4bd854764f9e
realestate-p2v <realestatephoto2video@gmail.com>
2026-02-06 19:06:25 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
       <DemoSection />
       <ProcessSection />
       <SaleBanner />
-      <GreenButton />
+     
     <TrustSection /> {/* Placed right under Hero */}
       
       <CTASection />
