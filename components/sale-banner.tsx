07f126773063c3a20f460cdf5cce78d04112c9a2
realestate-p2v <realestatephoto2video@gmail.com>
2026-02-24 20:52:41 -0500

    Update sale-banner.tsx

--- components/sale-banner.tsx
+++ components/sale-banner.tsx
@@ -141,7 +141,7 @@ export function SaleBanner() {
           </div>
           
           {/* Disclaimer */}
-          <p className="text-right text-[10px] text-slate-100/60">
+          <p className="text-right text-[10px] text-slate-100/80">
             *24-hour turnaround = 1 full business day
           </p>
           
