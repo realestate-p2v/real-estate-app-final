0fe6d021f3c666cd8133f37fbf44ad181e0fb3d0
realestate-p2v <realestatephoto2video@gmail.com>
2026-03-10 11:35:21 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import React from "react"
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
@@ -52,6 +53,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geist.className} font-sans antialiased`}>
+        <Script
+  id="meta-pixel"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1769748957035473');
+      fbq('track', 'PageView');
+    `,
+  }}
+/>
         {children}
         <PromoPopup />
         <Analytics />
