343e5c44a21a9f1a53824f66127b758a9ae7e74d
realestate-p2v <realestatephoto2video@gmail.com>
2026-03-15 16:39:30 -0600

    Update page.tsx

--- app/dashboard/videos/page.tsx
+++ app/dashboard/videos/page.tsx
@@ -357,7 +357,6 @@ export default function MyVideosPage() {
                               </Button>
                             )}
                           </div>
-                          )}
                         </div>
                       </div>
                     );
